acf4acd81bcddcf9d31fd2fc22fdbcda45b553cd
slee13-demo <slee13@atlassian.com>
2025-12-09 11:36:12 +0900

    Refactor for loops to use consistent syntax

--- 15.Alarm Clock/script.js
+++ 15.Alarm Clock/script.js
@@ -6,19 +6,19 @@ setAlarmBtn = document.querySelector("button");
 let alarmTime, isAlarmSet,
 ringtone = new Audio("./files/ringtone.mp3");
 
-for (let i = 12; i > 0; i--) {
+for (let i = 12; i > 0; i--) 
     i = i < 10 ? `0${i}` : i;
     let option = `<option value="${i}">${i}</option>`;
     selectMenu[0].firstElementChild.insertAdjacentHTML("afterend", option);
 }
 
-for (let i = 59; i >= 0; i--) {
+for (let i = 59; i >= 0; i--) 
     i = i < 10 ? `0${i}` : i;
     let option = `<option value="${i}">${i}</option>`;
     selectMenu[1].firstElementChild.insertAdjacentHTML("afterend", option);
 }
 
-for (let i = 2; i > 0; i--) {
+for (let i = 2; i > 0; i--) 
     let ampm = i == 1 ? "AM" : "PM";
     let option = `<option value="${ampm}">${ampm}</option>`;
     selectMenu[2].firstElementChild.insertAdjacentHTML("afterend", option);
